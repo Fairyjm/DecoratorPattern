12bdf1b13c91eb7283fb4ed29498bf37d6530770
Fairyjm <75209020+Fairyjm@users.noreply.github.com>
2021-05-27 10:45:10 +0800

    Add files via upload

---
+++ Decorator/src/DecoratorPattern/Finery.java
@@ -0,0 +1,29 @@
+package DecoratorPattern;
+/**
+ * 
+ * @author 15806
+ *∑˛ Œ¿‡
+ */
+public class Finery extends Person {
+private Person person;
+	
+	public Person getPerson() {
+		return person;
+	}
+ 
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+ 
+	//¥Ú∞Á
+	public void Decorate(Person person) {
+		this.person = person;
+	}
+ 
+	@Override
+	public void show() {
+		if (person != null) {
+			person.show();
+		}
+	}
+}
---
+++ Decorator/src/DecoratorPattern/Person.java
@@ -0,0 +1,21 @@
+package DecoratorPattern;
+/**
+ * 
+ * @author 15806
+ *具体构建角色[ConcreteComponent]
+ */
+public abstract class Person {
+	private String name;
+	 
+	public Person() {
+	}
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	public abstract void show();
+
+	
+
+}
